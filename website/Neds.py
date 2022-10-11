a1ec3286ddb9e7b13560d884f09c27295948fc06
dqueric <dqeric99@gmail.com>
2022-10-11 21:41:23 +1100

    renamed ladbrokes and neds

--- website/Neds.py
+++ website/Neds.py
@@ -4,21 +4,24 @@ from selenium.webdriver.common.by import By
 class Neds(WebScraper):
     def __init__(self, local):
         super().__init__(local)
-        self.source = "Neds"
+        self.source = "Ladbrokes"
 
     def scrape_data(self):
-        link = "https://www.ladbrokes.com.au/sports/esports/lo-l-worlds"
+        link = "https://www.neds.com.au/sports/esports/lo-l-worlds"
         self.driver.get(link)
         odds = [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''price-button-odds''')]
         teams = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''displayTitle''')]
 
 
-        link = "https://www.ladbrokes.com.au/sports/mma"
+        link = "https://www.neds.com.au/sports/mma"
         self.driver.get(link)
         odds += [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''price-button-odds''')]
-        teams += [i.text.split(' ')[1] for i in self.driver.find_elements(By.CLASS_NAME, '''displayTitle''')]
+        UpperCaseteam = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''displayTitle''')]
+        for team in UpperCaseteam:
+                TeamName = team.title()
+                teams.append(TeamName.split(" ")[1])
         self.data = [(teams[i], odds[i]) for i in range(len(teams))]
 
 if __name__ == "__main__":
-    scrape_obj = Neds(True)
+    scrape_obj = Ladbrokes(True)
     scrape_obj.write_to_csv()
