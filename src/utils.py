a846ec487d18508cb8d359f3b26d9681fd0fb3b7
Peter Kaltzis <pkal0002@student.monash.edu>
2022-10-23 13:40:25 +1100

    refactor

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+import glob
+from os import path
+from importlib import import_module
+
+
+def load_modules():
+    site_paths = glob.glob(path.join(path.dirname(__file__), '..', 'website', '*.py'))
+    site_list = [path.basename(site_path)[0:-3] for site_path in site_paths]
+    modules = [getattr(import_module('website.' + module), module) for module in site_list]
+    return modules
