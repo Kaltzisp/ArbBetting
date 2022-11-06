6bde72ee86fb424196561176cd582c358b22190c
dqueric <dqeric99@gmail.com>
2022-11-07 00:27:28 +1100

    moved around code

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.data import total_odds
+from src.arb import arb
+from src.bonus import bonus
+from src.mug import mug
+
+if __name__ == "__main__":
+    total_odds.main()
+    arb.main()
+    bonus.main()
+    mug.main()
