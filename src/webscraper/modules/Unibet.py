a901d112d6658749f3504b806e63ad9b916626f4
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-08 04:06:07 +1100

    converted to self.scrape format

--- src/webscraper/modules/Unibet.py
+++ src/webscraper/modules/Unibet.py
@@ -1,69 +1,25 @@
 from src.webscraper.WebScraper import WebScraper
-from selenium.webdriver.common.by import By
-import time
-import logging
+from src.utils import TEAM_ODDS, TEAM_NAME
 
 
 class Unibet(WebScraper):
     def __init__(self, driver=None):
         super().__init__(driver)
-        self.team_mapping = {
-            'MAD Lions': "MAD", 'DetonatioN FM': 'DFM', 'G2 Esports': 'G2',
-            'CTBC Flying Oyster': 'CFO', 'T1': 'T1', 'Edward Gaming': 'EDG',
-            'Top Esports': 'TES', 'DWG KIA': 'DK', 'GAM Esports': 'GAM',
-            '100 Thieves': '100', 'Cloud9': 'C9', 'JD Gaming': 'JDG',
-            'Rogue': 'RGE', 'Gen.G': 'GEN', 'Saigon Buffalo': 'SGB',
-            'LOUD': 'LLL', 'JD Gaming': 'JDG', 'DRX': 'DRX', 'Evil Geniuses': 'EG',
-            'Royal Never Give Up': 'RNG', 'Fnatic': 'FNC'
-        }
+        self.no_markets = r"Matches / Odds are not available at the moment"
+
+    def get_odds(self):
+        return [float(i) for i in self.find(rf"betButtonDisplay\"><div class=\"_20570\">.*?\"_278bc\".*?{TEAM_ODDS}<\/span>")]
+
+    def get_teams(self):
+        return self.find(rf"isExpandedView.*?teamName\">{TEAM_NAME}<")
 
     def scrape_data(self):
-        total_odds = []
-        total_teams = []
-
-        try:
-            link = "https://www.unibet.com.au/betting/sports/filter/ufc_mma/all/matches"
-            teams = []
-            self.driver.get(link)
-            time.sleep(3)
-            odds = [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''_8e013''')]
-            UFCUnibetteams = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''c539a''')]
-
-            for team in UFCUnibetteams:
-                if ',' in team:
-                    Name = team.split(", ")
-                    Name = Name[0]
-                else:
-                    Name = team
-                teams.append(Name)
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('UFC import failed')
-        total_odds += odds
-        total_teams += teams
-
-        try:
-            link = "https://www.unibet.com.au/betting/sports/filter/basketball/nba/all/matches"
-            self.driver.get(link)
-            time.sleep(3)
-            odds = [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''_8e013''')]
-            teams = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''c539a''')]
-            if len(teams) - 4 == len(odds):
-                teams = teams[2:]
-                teams = teams[:4] + teams[6:]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('NBA import failed')
-        total_odds += odds
-        total_teams += teams
-
-        self.data = [(total_teams[i], total_odds[i]) for i in range(len(total_teams))]
+        self.scrape("https://www.unibet.com.au/betting/sports/filter/basketball/nba/all/matches")
+        self.scrape("https://www.unibet.com.au/betting/sports/filter/american_football/nfl/all/matches")
+        self.scrape("https://www.unibet.com.au/betting/sports/filter/tennis/challenger/bratislava/all/matches", name_index=0)
+        self.scrape("https://www.unibet.com.au/betting/sports/filter/tennis/wta/colina/all/matches", name_index=0)
+        self.scrape("https://www.unibet.com.au/betting/sports/filter/ufc_mma/ufc/all/matches", name_index=0)
+        self.data = [(self.total_teams[i], self.total_odds[i]) for i in range(len(self.total_teams))]
 
 
 if __name__ == "__main__":
