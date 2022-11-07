f01f9cc02a20103aa2ef8866eb501f0b78919ba1
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-08 02:06:37 +1100

    move utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,7 @@ def load_modules():
     site_list = [path.basename(site_path)[0:-3] for site_path in site_paths]
     modules = [getattr(import_module("src.webscraper.modules." + module), module) for module in site_list]
     return modules
+
+
+TEAM_ODDS = r"([\d\.]+)"
+TEAM_NAME = r"([\w\-\/', ]+)"
