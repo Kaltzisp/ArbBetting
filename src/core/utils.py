304cb2408f97de69c3bef79f49bce03ff2e6278c
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-08 17:17:23 +1100

    updated utils.py

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,14 +1,2 @@
-from os import path
-from glob import glob
-from importlib import import_module
-
-
-def load_modules():
-    site_paths = glob(path.join(path.dirname(__file__), "webscraper", "modules", "*.py"))
-    site_list = [path.basename(site_path)[0:-3] for site_path in site_paths]
-    modules = [getattr(import_module("src.webscraper.modules." + module), module) for module in site_list]
-    return modules
-
-
 TEAM_ODDS = r"([\d\.]+)"
 TEAM_NAME = r"([\w\-\/', ]+)"
