8c47d3716375988ee9ded295ab7e5428cef08db7
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-08 20:01:35 +1100

    updated main.py imports

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from src.arb import arb
-from src.bonus import bonus
-from src.mug import mug
 from src.core.scrape_odds import scrape_odds
 from src.core.combine_odds import combine_odds
+from src.core import list_arbs, list_bonuses, list_mugs
+
 
 if __name__ == "__main__":
     scrape_odds()
     combine_odds()
-    arb.main()
-    bonus.main(bonus_amount=100, bonus_source="Betright")
-    mug.main()
+    list_arbs.main()
+    list_bonuses.main(bonus_amount=100, bonus_source="Betr")
+    list_mugs.main()
