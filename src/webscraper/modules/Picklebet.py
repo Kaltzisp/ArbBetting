9a59cdcab5c55285986106ed7a2dbef998548527
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-09 20:40:24 +1100

    refactored Picklebet scraper

--- src/webscraper/modules/Picklebet.py
+++ src/webscraper/modules/Picklebet.py
@@ -1,59 +1,22 @@
 from src.webscraper.WebScraper import WebScraper
-from selenium.webdriver.common.by import By
-import logging
+from src.core.utils import TEAM_ODDS, TEAM_NAME
 
 
 class Picklebet(WebScraper):
     def __init__(self, driver=None, hidden=False):
         super().__init__(driver, hidden)
-        self.team_mapping = {
-            'MAD Lions': "MAD", 'DetonatioN FM': 'DFM', 'G2 Esports': 'G2',
-            'CTBC Flying Oyster': 'CFO', 'T1 Esports': 'T1', 'EDward Gaming': 'EDG',
-            'Top Esports': 'TES', 'DAMWON Gaming': 'DK', 'GAM Esports': 'GAM',
-            '100 Thieves': '100', 'Cloud9': 'C9', 'JD Gaming': 'JDG',
-            'Rogue': 'RGE', 'Gen.G': 'GEN', 'Saigon Buffalo': 'SGB',
-            'LOUD': 'LLL', 'JD Gaming': 'JDG', 'DRX': 'DRX', 'Royal Never Give Up': 'RNG',
-            'Evil Geniuses': 'EG', 'Fnatic': 'FNC', 'Team Secret': 'Secret', 'Virtus.Pro': 'VP'
-        }
+        self.no_markets = r"There are currently no active markets."
+
+    def get_odds(self):
+        return [float(i) for i in self.find(rf"Outcome-module--odds--onB1v\">{TEAM_ODDS}<\/div>")]
+
+    def get_teams(self):
+        return self.find(rf"Outcome-module--name--DgQM8\">{TEAM_NAME}<\/div>")
 
     def scrape_data(self):
-        total_odds = []
-        total_teams = []
-
-        try:
-            link = "https://picklebet.com/sports/mma/betting?page=1&tab=next"
-            self.driver.get(link)
-            MMAPickleteams = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''Outcome-module--name--DgQM8''')]
-            teams = []
-            for team in MMAPickleteams:
-                Name = team.split(", ")
-                Name = Name[0]
-                teams.append(Name)
-            odds = [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''Outcome-module--odds--onB1v''')]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('UFC import failed')
-        total_odds += odds
-        total_teams += teams
-
-        try:
-            link = "https://picklebet.com/sports/basketball/betting?page=1&tab=next&tour=BASKETBALL%3ANBA"
-            self.driver.get(link)
-            teams = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''Outcome-module--name--DgQM8''')]
-            odds = [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''Outcome-module--odds--onB1v''')]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('NBA import failed')
-        total_odds += odds
-        total_teams += teams
-
-        self.data = [(total_teams[i], total_odds[i]) for i in range(len(total_teams))]
+        self.scrape("https://picklebet.com/sports/mma/betting/?page=1&tab=next", name_index=0)
+        self.scrape("https://picklebet.com/sports/basketball/betting?page=1&tab=next&tour=BASKETBALL%3ANBA")
+        self.data = [(self.total_teams[i], self.total_odds[i]) for i in range(len(self.total_teams))]
 
 
 if __name__ == "__main__":
