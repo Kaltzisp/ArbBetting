23cf51725e27b1607cd13946a449b66cc2eb3e80
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-09 23:25:33 +1100

    refactored Sportsbet scraper

--- src/webscraper/modules/Sportsbet.py
+++ src/webscraper/modules/Sportsbet.py
@@ -1,67 +1,21 @@
 from src.webscraper.WebScraper import WebScraper
-from selenium.webdriver.common.by import By
-import logging
-
+from src.core.utils import TEAM_ODDS, TEAM_NAME
 
 class Sportsbet(WebScraper):
     def __init__(self, driver=None, hidden=False):
         super().__init__(driver, hidden)
-        self.team_mapping = {
-            'MAD Lions': "MAD", 'DetonatioN FM': 'DFM', 'G2 Esports': 'G2',
-            'CTBC Flying Oyster': 'CFO', 'T1': 'T1', 'Edward Gaming': 'EDG',
-            'Top Esports': 'TES', 'DWG KIA': 'DK', 'GAM Esports': 'GAM',
-            '100 Thieves': '100', 'Cloud9': 'C9', 'JD Gaming': 'JDG',
-            'Rogue': 'RGE', 'Gen.G': 'GEN', 'Saigon Buffalo': 'SGB',
-            'LOUD': 'LLL', 'JD Gaming': 'JDG', 'DRX': 'DRX', 'Evil Geniuses': 'EG',
-            'Royal Never Give Up': 'RNG', 'Fnatic': 'FNC'
-        }
+        self.no_markets = r"NO MARKETS AVAILABLE"
+
+    def get_odds(self):
+        return [float(i) for i in self.find(rf"<span .*?price-text\">{TEAM_ODDS}<\/span>")]
+
+    def get_teams(self):
+        return self.find(rf"(?:outcome-name|participantText).*?>{TEAM_NAME}<\/(?:span|div)>")
 
     def scrape_data(self):
-        total_odds = []
-        total_teams = []
-
-        try:
-            link = "https://www.sportsbet.com.au/betting/ufc-mma"
-            self.driver.get(link)
-            raw_odds = [i.text for i in self.driver.find_elements(By.XPATH, '''//span[@class='size14_f7opyze bold_f1au7gae priceTextSize_frw9zm9']''')]
-            odds = []
-            for odd in raw_odds:
-                if odd == 'SUSP':
-                    odds.append(0)
-                else:
-                    odds.append(float(odd))
-            ufc_names = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''size12_fq5j3k2''')][2:]
-            teams = [name.split(' ')[1] for name in ufc_names]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('UFC import failed')
-        total_odds += odds
-        total_teams += teams
-
-        try:
-            link = "https://www.sportsbet.com.au/betting/basketball-us"
-            self.driver.get(link)
-            raw_odds = [i.text for i in self.driver.find_elements(By.XPATH, '''//span[@class='size14_f7opyze bold_f1au7gae priceTextSize_frw9zm9']''')]
-            odds = []
-            for odd in raw_odds:
-                if odd == 'SUSP':
-                    odds.append(0)
-                else:
-                    odds.append(float(odd))
-            teams = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''size12_fq5j3k2''')][2:]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('NBA import failed')
-        total_odds += odds
-        total_teams += teams
-
-        self.data = [(total_teams[i], total_odds[i]) for i in range(len(total_teams))]
+        self.scrape("https://www.sportsbet.com.au/betting/ufc-mma", name_index=-1)
+        self.scrape("https://www.sportsbet.com.au/betting/basketball-us")
+        self.data = [(self.total_teams[i], self.total_odds[i]) for i in range(len(self.total_teams))]
 
 
 if __name__ == "__main__":
