4969d8a2d4cefb437cb24450a0d382f1d8660ec6
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-09 20:30:41 +1100

    refactored Palmerbet scraper

--- src/webscraper/modules/Palmerbet.py
+++ src/webscraper/modules/Palmerbet.py
@@ -1,77 +1,22 @@
 from src.webscraper.WebScraper import WebScraper
-from selenium.webdriver.common.by import By
-import time
-import logging
+from src.core.utils import TEAM_ODDS, TEAM_NAME
 
 
 class Palmerbet(WebScraper):
     def __init__(self, driver=None, hidden=False):
         super().__init__(driver, hidden)
-        self.team_mapping = {
-            'MAD Lions': "MAD", 'DetonatioN FM': 'DFM', 'G2 Esports': 'G2',
-            'CTBC Flying Oyster': 'CFO', 'T1': 'T1', 'Edward Gaming': 'EDG',
-            'Top Esports': 'TES', 'DWG KIA': 'DK', 'GAM Esports': 'GAM',
-            '100 Thieves': '100', 'Cloud9': 'C9', 'JD Gaming': 'JDG',
-            'Rogue': 'RGE', 'Gen.G Esports': 'GEN', 'Saigon Buffalo': 'SGB',
-            'LOUD': 'LLL', 'JD Gaming': 'JDG', 'DRX': 'DRX', 'Evil Geniuses': 'EG',
-            'Royal Never Give Up': 'RNG', 'Fnatic': 'FNC'
-        }
+        self.no_markets = r"NO MARKETS AVAILABLE"
 
-    def is_float(self, element) -> bool:
-        try:
-            float(element)
-            return True
-        except ValueError:
-            return False
+    def get_odds(self):
+        return [float(i) for i in self.find(rf"alt-button-design.*?>{TEAM_ODDS}<\/div>")]
+
+    def get_teams(self):
+        return self.find(rf"class=\"team-name\"> {TEAM_NAME} <\/span>")
 
     def scrape_data(self):
-        total_odds = []
-        total_teams = []
-
-        try:
-            link = "https://www.palmerbet.com/sports/martial-arts/Ultimate%20Fighting%20Championship/7ee8e39f-6bec-45c1-b484-28023ce0dfce"
-            self.driver.get(link)
-            while True:
-                time.sleep(3)
-                try:
-                    ufc_odds = [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''ng-star-inserted''') if self.is_float(i.text)]
-                    break
-                except Exception as e:
-                    logging.exception(e)
-            odds = ufc_odds[::3]
-            ufc_names = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''team-name''')][:len(odds)]
-            teams = [name.split(",")[0] for name in ufc_names]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('UFC import failed')
-        total_odds += odds
-        total_teams += teams
-
-        try:
-            link = '''https://www.palmerbet.com/sports/basketball/NBA/b26e5acc-02ff-4b22-ae69-0491fbd2500e'''
-            self.driver.get(link)
-            while True:
-                time.sleep(3)
-                try:
-                    odds = [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''ng-star-inserted''') if self.is_float(i.text)]
-                    break
-                except Exception as e:
-                    logging.exception(e)
-            odds = odds[::3]
-            teams = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''team-name''')][:len(odds)]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('NBA import failed')
-        total_odds += odds
-        total_teams += teams
-
-        self.data = [(total_teams[i], total_odds[i]) for i in range(len(total_teams))]
+        self.scrape("https://www.palmerbet.com/sports/martial-arts/Ultimate%20Fighting%20Championship/7ee8e39f-6bec-45c1-b484-28023ce0dfce", name_index=0)
+        self.scrape("https://www.palmerbet.com/sports/basketball/NBA/b26e5acc-02ff-4b22-ae69-0491fbd2500e")
+        self.data = [(self.total_teams[i], self.total_odds[i]) for i in range(len(self.total_teams))]
 
 
 if __name__ == "__main__":
