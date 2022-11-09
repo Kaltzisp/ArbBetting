09cdab4962a9534cfd316b948f472fb591c0f36c
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-09 13:17:41 +1100

    refactored Betright scraper

--- src/webscraper/modules/Betright.py
+++ src/webscraper/modules/Betright.py
@@ -5,12 +5,13 @@ from src.core.utils import TEAM_ODDS, TEAM_NAME
 class Betright(WebScraper):
     def __init__(self, driver=None, hidden=False):
         super().__init__(driver, hidden)
+        self.no_markets = r"NO MARKETS AVAILABLE"
 
     def get_odds(self):
         return [float(i) for i in self.find(rf"<span class=\"place-bet__odds ng-binding\">{TEAM_ODDS}<\/span>")]
 
     def get_teams(self):
-        return self.find(rf"<div class=\"zeta headline-wrap ng-binding\">{TEAM_NAME}<\/div>")
+        return self.find(rf"headline-wrap ng-binding\">{TEAM_NAME}<\/div>")
 
     def scrape_data(self):
         self.scrape("https://www.betright.com.au/sports/Martial-Arts/128", name_index=-1)
