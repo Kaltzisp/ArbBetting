3c73ebe070fb65bf92377b482176da59ecef8742
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-09 20:14:02 +1100

    refactored Elitebet scraper

--- src/webscraper/modules/Elitebet.py
+++ src/webscraper/modules/Elitebet.py
@@ -1,58 +1,22 @@
 from src.webscraper.WebScraper import WebScraper
-import re
-import time
-import logging
+from src.core.utils import TEAM_ODDS, TEAM_NAME
 
 
 class Elitebet(WebScraper):
     def __init__(self, driver=None, hidden=False):
         super().__init__(driver, hidden)
-        self.team_mapping = {
-            'MAD Lions': "MAD", 'DetonatioN FM': 'DFM', 'G2 Esports': 'G2',
-            'CTBC Flying Oyster': 'CFO', 'T1': 'T1', 'EDward Gaming': 'EDG',
-            'Top Esports': 'TES', 'DWG KIA': 'DK', 'GAM Esports': 'GAM',
-            '100 Thieves': '100', 'Cloud9': 'C9', 'JD Gaming': 'JDG',
-            'Rogue': 'RGE', 'Gen.G': 'GEN', 'Saigon Buffalo': 'SGB',
-            'LOUD': 'LLL', 'JD Gaming': 'JDG', 'DRX': 'DRX', 'Evil Geniuses': 'EG',
-            'Royal Never Give Up': 'RNG', 'Fnatic': 'FNC', 'GAM Sports': 'GAM'
-        }
+        self.no_markets = r"NO MARKETS AVAILABLE"
+
+    def get_odds(self):
+        return [float(i) for i in self.find(rf"<span>{TEAM_ODDS}<\/span>")]
+
+    def get_teams(self):
+        return self.find(rf"css-1pex9yp\">{TEAM_NAME}<\/p>")
 
     def scrape_data(self):
-        total_odds = []
-        total_teams = []
-
-        try:
-            link = '''https://www.elitebet.com.au/sports/Basketball/NBA/BASK'''
-            self.driver.get(link)
-            time.sleep(3)
-            odds = [float(i) for i in re.findall('''css-147k6hk">&nbsp;<span>([\d\.]*)<''', self.driver.page_source)]
-            teams = re.findall('''MuiTypography-root MuiTypography-body1 MuiTypography-alignCenter MuiTypography-noWrap css-1pex9yp">([\w\d\. ]*)<''', self.driver.page_source)
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('NBA import failed')
-        total_odds += odds
-        total_teams += teams
-
-        try:
-            link = '''https://www.elitebet.com.au/sports/Mixed_Martial_Arts/MMA'''
-            self.driver.get(link)
-            time.sleep(3)
-            odds = [float(i) for i in re.findall('''css-147k6hk">&nbsp;<span>([\d\.]*)<''', self.driver.page_source)]
-            teams = re.findall('''MuiTypography-root MuiTypography-body1 MuiTypography-alignCenter MuiTypography-noWrap css-1pex9yp">([\w\d\. ]*)<''', self.driver.page_source)
-            teams = [team.split(' ')[-1] for team in teams]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('UFC import failed')
-        total_odds += odds
-        total_teams += teams
-
-        self.data = [(total_teams[i], total_odds[i]) for i in range(len(total_teams))]
+        self.scrape("https://www.elitebet.com.au/sports/Basketball/NBA/BASK")
+        self.scrape("https://www.elitebet.com.au/sports/Mixed_Martial_Arts/MMA", name_index=-1)
+        self.data = [(self.total_teams[i], self.total_odds[i]) for i in range(len(self.total_teams))]
 
 
 if __name__ == "__main__":
