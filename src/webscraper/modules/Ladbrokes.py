386ff4caf1342b3ca69cb9257f80ed4da87271a3
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-09 13:31:29 +1100

    refactored Ladbrokes scraper

--- src/webscraper/modules/Ladbrokes.py
+++ src/webscraper/modules/Ladbrokes.py
@@ -1,64 +1,24 @@
 from src.webscraper.WebScraper import WebScraper
-from selenium.webdriver.common.by import By
-import logging
+from src.core.utils import TEAM_ODDS, TEAM_NAME
 
 
 class Ladbrokes(WebScraper):
     def __init__(self, driver=None, hidden=False):
         super().__init__(driver, hidden)
+        self.no_markets = r"NO MARKETS AVAILABLE"
+
+    def get_odds(self):
+        return [float(i) for i in self.find(rf"<span data-testid=\"price-button-odds\" has-boost=\"true\">{TEAM_ODDS}<\/span>")]
+
+    def get_teams(self):
+        return self.find(rf"<span class=\"displayTitle\">{TEAM_NAME}<\/span>")
 
     def scrape_data(self):
-        total_odds = []
-        total_teams = []
-
-        try:
-            link = "https://www.ladbrokes.com.au/sports/mma"
-            self.driver.get(link)
-            odds = [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''price-button-odds''')]
-            teams = [i.text.split(' ')[1] for i in self.driver.find_elements(By.CLASS_NAME, '''displayTitle''')]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('UFC import failed')
-        total_odds += odds
-        total_teams += teams
-
-        try:
-            link = "https://www.ladbrokes.com.au/sports/basketball/usa/nba"
-            self.driver.get(link)
-            odds = [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''price-button-odds''')]
-            teams = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''displayTitle''')]
-            lines = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''displayLine''') if i.text != '']
-            odds = odds[:len(odds)-len(lines)]
-            teams = teams[:len(teams)-len(lines)]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('NBA import failed')
-        total_odds += odds
-        total_teams += teams
-
-        self.data = [(total_teams[i], total_odds[i]) for i in range(len(total_teams))]
-
-        # n_game_elements = len([i for i in self.driver.find_elements(By.CLASS_NAME, '''sports-event-title__name-text''')])
-        # for idx in range(n_game_elements):
-        #     game = self.driver.find_elements(By.CLASS_NAME, '''sports-event-title__name-text''')[idx]
-        #     game.click()
-        #     self.driver.find_element(By.XPATH, '''//*[@id="25bf3cd2-c56e-4b41-8a73-ccad21982ab3"]/div[1]/h3/span''').click()
-        #     self.driver.find_element(By.XPATH, '''//*[@id="f58b187d-939e-448f-8e0c-b9c216c959d3"]/div[1]/h3/span''').click()
-        #     self.driver.find_element(By.XPATH, '''//*[@id="77d78111-48ba-4c5e-b605-d4ad29f5b441"]/div[1]/h3/span''').click()
-        #     first_blood_id = re.findall('''id="(accordion_[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12})"[^<>]*> <[^<>]*><[^<>]*> <[^<>]*><[^<>]*>First Blood<''', self.driver.page_source)[0]
-        #     first_inhib_id = re.findall('''id="(accordion_[\w\d]{8}-[\w\d]{4}-[\w\d]{4}-[\w\d]{4}-[\w\d]{12})"[^<>]*> <[^<>]*><[^<>]*> <[^<>]*><[^<>]*>Team to destroy First Inhibitor<''', self.driver.page_source)[0]
-        #     self.driver.find_element(By.XPATH, f'''//*[@id="{first_blood_id}"]/div[1]/h3/span''').click()
-        #     self.driver.find_element(By.XPATH, f'''//*[@id="{first_inhib_id}"]/div[1]/h3/span''').click()
-        #     odds += [float(i.text) for i in self.driver.find_elements(By.CLASS_NAME, '''price-button-odds''') if i.text != ''][2:]
-        #     teams += [i.text + ' First Inhib' for i in self.driver.find_elements(By.CLASS_NAME, '''market-two-col__entrant-name''') if i.text != ''][:2]
-        #     teams += [i.text + ' First Blood' for i in self.driver.find_elements(By.CLASS_NAME, '''market-two-col__entrant-name''') if i.text != ''][2:4]
-        #     self.driver.back()
+        self.scrape("https://www.ladbrokes.com.au/sports/mma/ufc-281-adesanya-vs-pereira", name_index=-1)
+        self.scrape("https://www.ladbrokes.com.au/sports/mma/ufc-fight-night-spivak-vs-lewis", name_index=-1)
+        self.scrape("https://www.ladbrokes.com.au/sports/mma/ufc-282-teixeira-vs-prochazka", name_index=-1)
+        self.scrape("https://www.ladbrokes.com.au/sports/basketball/usa/nba")
+        self.data = [(self.total_teams[i], self.total_odds[i]) for i in range(len(self.total_teams))]
 
 
 if __name__ == "__main__":
