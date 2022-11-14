d3c0a7db57714b5d0e3db93ca49bd1f93320eb72
dqueric <dqeric99@gmail.com>
2022-11-14 16:25:32 +1100

    changed place solve lay

--- src/horse/place_solve_lay.py
+++ src/horse/place_solve_lay.py
@@ -2,7 +2,7 @@ import numpy as np
 from itertools import combinations
 import sympy
 
-backs = np.array([1.35, 1.55, 3.90, 5.00, 5.00, 1.70, 2.60, 2.60, 4.60, 6.50])
+backs = np.array([6, 1.28, 2.3, 1.28, 6.5, 5])
 horses = [i for i in range(len(backs))]
 win_list = [[0] + list(i) for i in combinations(horses[1:], 2)]
 loss_list = [list(i) for i in combinations(horses[1:], 3)]
@@ -15,16 +15,8 @@ loss_list = [list(i) for i in combinations(horses[1:], 3)]
 raw_a = []
 raw_b = []
 
-for i in range(len(win_list) - 1):
-    row_1 = [0] * len(horses)
-    row_2 = [0] * len(horses)
-    for horse in win_list[i]:
-        row_1[horse] = backs[horse]
-    for horse in win_list[i+1]:
-        row_2[horse] = backs[horse]
-    row = [row_1[i]-row_2[i] for i in range(len(horses))]
-    raw_a.append(row)
-    raw_b.append(0)
+raw_a.append([1] + [0 for i in range(len(horses)-1)])
+raw_b.append(0)
 
 for i in range(len(loss_list) - 1):
     row_1 = [0] * len(horses)
