b4e36b97367c5507501a7dd7b38015461663eb5f
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-17 16:25:57 +1100

    refactored Tab scraper

--- src/webscraper/modules/Tab.py
+++ src/webscraper/modules/Tab.py
@@ -1,90 +1,24 @@
 from src.webscraper.WebScraper import WebScraper
-from selenium.webdriver.common.by import By
-import logging
+from src.core.utils import TEAM_ODDS, TEAM_NAME
 
 
 class Tab(WebScraper):
     def __init__(self, driver=None, hidden=False):
         super().__init__(driver, hidden)
-        self.team_mapping = {
-            'MAD Lions': "MAD", 'DetonatioN FM': 'DFM', 'G2': 'G2',
-            'CTBC Flying Oyster': 'CFO', 'T1 Esports': 'T1', 'EDward Gaming': 'EDG',
-            'Top Esports': 'TES', 'Dwg Kia': 'DK', 'GAM Esports': 'GAM',
-            '100 Thieves': '100', 'Cloud9': 'C9', 'JD Gaming': 'JDG',
-            'Rogue': 'RGE', 'Gen.g': 'GEN', 'Saigon Buffalo': 'SGB',
-            'LOUD': 'LLL', 'Jd Gaming': 'JDG', 'Drx': 'DRX', 'Royal Never Give Up': 'RNG',
-            'Evil Geniuses': 'EG', 'Fnatic': 'FNC', 'Gam': 'GAM', 'Gen.g': 'GEN', 'Edward Gaming': 'EDG',
-            'Royal Never Give': 'RNG', 'T1': 'T1', 'Ctbc Flying Oyst': 'CFO'
-        }
-        self.nba_mapping = {
-            'Boston': 'Boston Celtics',
-            'Philadelphia': 'Philadelphia 76ers',
-            'Golden State': 'Golden State Warriors',
-            'LA Lakers': 'Los Angeles Lakers',
-            'Indiana': 'Indiana Pacers',
-            'Washington': 'Washington Wizards',
-            'Detroit': 'Detroit Pistons',
-            'Orlando': 'Orlando Magic',
-            'Toronto': 'Toronto Raptors',
-            'Cleveland': 'Cleveland Cavaliers',
-            'Brooklyn': 'Brooklyn Nets',
+        self.no_markets = r"NO MARKETS AVAILABLE"
 
-            'New York': 'New York Knicks',
-            'Atlanta': 'Atlanta Hawks',
-            'Houston': 'Houston Rockets',
-            'Memphis': 'Memphis Grizzlies',
-            'Miami': 'Miami Heat',
-            'Chicago': 'Chicago Bulls',
-            'Minnesota': 'Minnesota Timberwolves',
-            'Oklahoma City': 'Oklahoma City Thunder',
-            'San Antonio': 'San Antonio Spurs',
-            'Charlotte': 'Charlotte Hornets',
-            'Utah': 'Utah Jazz',
-            'Denver': 'Denver Nuggets',
-            'Sacramento': 'Sacramento Kings',
-            'Portland': 'Portland Trail Blazers',
-            'Phoenix': 'Phoenix Suns',
-            'Dallas': 'Dallas Mavericks',
-            'Milwaukee': 'Milwaukee Bucks',
-            'New Orleans': 'New Orleans Pelicans',
-            'LA Clippers': 'Los Angeles Clippers'
-        }
+    def get_odds(self):
+        return [float(i) for i in self.find(rf"data-content=\"Head To Head\"(?:.|\n)*?WithoutAnimation\s}}\" style=\"\">{TEAM_ODDS}<\/div>")]
+
+    def get_teams(self):
+        matches = self.find(rf"match-name-text\">{TEAM_NAME} <\/span>")
+        teams = []
+        for match in matches:
+            teams.extend(match.split(" v "))
+        return teams
 
     def scrape_data(self):
-        total_teams = []
-        total_odds = []
-
-        try:
-            link = "https://www.tab.com.au/sports/betting/Basketball/competitions/NBA"
-            self.driver.get(link)
-            raw_odds = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''animate-odd''')]
-            odds = []
-            for odd in raw_odds:
-                if odd == "SUSP":
-                    odds.append(0)
-                else:
-                    odds.append(float(odd))
-            odds = [odds[i] for i in range(len(odds)) if i % 4 in [1, 2]]
-            NameVsName = [i.text for i in self.driver.find_elements(By.CLASS_NAME, '''match-name-text''')]
-            teams = []
-
-            for Name in NameVsName:
-                TeamName = Name.split(" v ")
-                Team1 = TeamName[0]
-                Team2 = TeamName[1]
-                teams.append(Team1)
-                teams.append(Team2)
-            teams = [self.nba_mapping[team] for team in teams]
-            assert(len(odds) == len(teams))
-        except Exception as e:
-            odds = []
-            teams = []
-            logging.exception(e)
-            logging.info('NBA import failed')
-        total_odds += odds
-        total_teams += teams
-
-        self.data = [(total_teams[i], total_odds[i]) for i in range(len(total_teams))]
+        self.scrape("https://www.tab.com.au/sports/betting/Basketball/competitions/NBA")
 
 
 if __name__ == "__main__":
