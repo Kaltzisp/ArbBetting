86efe77c4ccd4468ce7d10faeb909d98540f1267
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-17 16:26:20 +1100

    moved TAB mappings to utils.py

--- src/core/utils.py
+++ src/core/utils.py
@@ -15,4 +15,35 @@ class log():
 TEAM_ODDS = r"([\d\.]+)"
 TEAM_NAME = r"([\w\-\/',. ]+)"
 
-TEAM_MAPPING = {"LA Clippers": "Los Angeles Clippers"}
+TEAM_MAPPING = {
+    "Atlanta": "Atlanta Hawks",
+    "Boston": "Boston Celtics",
+    "Brooklyn": "Brooklyn Nets",
+    "Charlotte": "Charlotte Hornets",
+    "Chicago": "Chicago Bulls",
+    "Cleveland": "Cleveland Cavaliers",
+    "Dallas": "Dallas Mavericks",
+    "Denver": "Denver Nuggets",
+    "Detroit": "Detroit Pistons",
+    "Golden State": "Golden State Warriors",
+    "Houston": "Houston Rockets",
+    "Indiana": "Indiana Pacers",
+    "LA Clippers": "Los Angeles Clippers",
+    "LA Lakers": "Los Angeles Lakers",
+    "Memphis": "Memphis Grizzlies",
+    "Miami": "Miami Heat",
+    "Milwaukee": "Milwaukee Bucks",
+    "Minnesota": "Minnesota Timberwolves",
+    "New Orleans": "New Orleans Pelicans",
+    "New York": "New York Knicks",
+    "Oklahoma City": "Oklahoma City Thunder",
+    "Orlando": "Orlando Magic",
+    "Philadelphia": "Philadelphia 76ers",
+    "Phoenix": "Phoenix Suns",
+    "Portland": "Portland Trail Blazers",
+    "Sacramento": "Sacramento Kings",
+    "San Antonio": "San Antonio Spurs",
+    "Toronto": "Toronto Raptors",
+    "Utah": "Utah Jazz",
+    "Washington": "Washington Wizards"
+}
