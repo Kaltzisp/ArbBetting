9654b64fd0098217415d5ddff6974cc5c8b3ffc9
Peter Kaltzis <pkal0002@student.monash.edu>
2022-11-17 16:51:31 +1100

    updated main.py defaults

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from src.core.scrape_odds import scrape_odds
 from src.core.combine_odds import combine_odds
-from src.core import list_arbs, list_bonuses, list_mugs
+from src.core import list_arbs, list_bonuses, list_mugs, list_multi
 
 
 if __name__ == "__main__":
     scrape_odds(hidden=False)
     combine_odds()
     list_arbs.main()
-    list_bonuses.main(bonus_source="Betr", bonus_amount=328.50)
-    list_mugs.main(mug_source="Rivalry", mug_amount=100)
+    list_bonuses.main(bonus_source="Betr", bonus_amount=100)
+    list_mugs.main(mug_source="Betr", mug_amount=100)
+    list_multi.main(source="Betr", amount=50)
